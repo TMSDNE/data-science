6f860d68d64c700ff4670d223c817e395373f8cc
Cole Hudson <cole@colejhudson.com>
2019-04-16 15:25:31 -0700

    added plotly table

--- schemas/tables.sql
+++ schemas/tables.sql
@@ -2,4 +2,10 @@ create table if not exists Summary(
   id integer primary key,
   Commentary varchar(3000)
   Date varchar(10),
+); 
+
+create table if not exists Plotly(
+  id integer primary key,
+  Date varchar(10),
+  Link varchar(255)
 );
