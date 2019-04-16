7074aca4505083757cec07183035c4c763bc8b63
Cole Hudson <cole@colejhudson.com>
2019-04-16 14:51:15 -0700

    added backend, POST support, schema to yielded json

--- app.py
+++ app.py
@@ -1,8 +1,50 @@
+import collections
+import pymysql
 import flask
+import json
+import os
+
+HOST = os.environ.get("DB_HOST")
+PORT = int(os.environ.get("DB_PORT"))
+NAME = os.environ.get("DB_NAME")
+USER = os.environ.get("DB_USER")
+PASS = os.environ.get("DB_PASS")
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+fields = ["date", "commentary"]
+columns = ', '.join(fields)
+Summary = collections.namedtuple("Summary", fields)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    data = { 'text': 'Hello World' }
-    return flask.jsonify(data) 
+    res = { 'successful': False, 'results': [] }
+
+    try:
+        with pymysql.connect(HOST, user=USER, port=PORT, passwd=PASS, db=NAME) as db:
+            req = flask.request
+            condition = ''
+
+            template = '''
+                select {}
+                from Summary
+                {}
+                order by rand()
+                limit 1;
+            '''
+
+            if req.method == 'POST':
+                data = json.loads(str(req.data, 'ascii'))
+                condition = 'where Date = "{}"'.format(data['date'])
+
+            query = template.format(columns, condition)
+            db.execute(query)
+
+            res['results'] = [Summary(*result)._asdict() for result in list(db.fetchall())]
+            res['successful'] = True
+
+    except Exception as e:
+        print(e)
+        pass
+
+    return flask.jsonify(res)
